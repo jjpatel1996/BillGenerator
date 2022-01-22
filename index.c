796560f35bd5eca0b7ca8999e837f5bcff6aac6b
Jay Patel <jjpatel1996@gmail.com>
2022-01-22 22:34:31 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//functions to generate bills 
+void generateBillheader(char name[50],char date[30]){
+printf("\n \n");
+      printf("\t   ADV. Restaurant");
+      printf("\n\t  -----------------");
+      printf("\ndate:%s",date);
+      printf("\nInvoice To: %s",name);
+      printf("\n");
+      printf("--------------------------------------------\n");
+      printf("Items\t\t");
+      printf("Qty\t\t");
+      printf("Total\t\t");
+      printf("\n------------------------------------------");
+printf("\n\n");
+}
+void generateBillBody(char item[30],int qty,float price){
+      printf("%s\t\t",item);
+      printf("%d\t\t",qty);
+	  printf("%.2f\t\t",qty * price);
+	  printf("\n");
+}
+void generateBillFooter(float total){
+      printf("\n");
+	  float dis = 0.1*total;
+	  float netTotal=total-dis;
+	  float cgst=0.09*netTotal,grandTotal=netTotal + 2*cgst;
+	  printf("----------------------------------------------\n");
+	  printf("Sub Total\t\t\t%.2f",total);
+      printf("\nDiscount @10%s\t\t\t%.2f","%",dis);
+	  printf("\n\t\t\t\t-------");
+      printf("\nNet Total\t\t\t%.2f",netTotal);
+      printf("\nCGST @9%s\t\t\t%.2f","%",cgst);
+      printf("\nSGST @9%s\t\t\t%.2f","%",cgst);
+	  printf("\n----------------------------------------");
+	  printf("\nGrand Total\t\t\t %.2f",grandTotal);
+	  printf("\n----------------------------------------\n");
+	  }
+	  int main(){
+	  //dashboard
+	  printf("\t==================ADV.RESTAURANT==================");
+	  printf("\n1.Generate Invoice");
+	  printf("\n2.Show all Invoices");
+	  printf("\n3.Search invoice");
+	  printf("\n4.Exit");
+	  
+	  printf("\n\n");
+	  
+	  return 0;
+	  }
